dc3c85173c3c4b670db67c64dc11ff3d254dea63
Atul Bhatt <atulbhatt61@gmail.com>
2025-02-20 08:28:49 +0530

    search srt file for caption

--- main.go
+++ main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"os"
 	"log"
+	"bufio"
+	"fmt"
+	"strings"
 )
 
+/* -- subtitle format --
+43
+00:00:02,462 --> 00:00:02,493
+Evan told me
+you didn't get into Dartmouth.
+*/
+
 func main() {
-	b, err := os.ReadFile("caption.txt")
+	file, err := os.Open("superbad.srt")
+	defer file.Close()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 	
-	os.Stdout.Write(b)
+	// Read user input for caption
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		lines := bufio.NewScanner(file)
+		for lines.Scan() {
+			if strings.Contains(lines.Text(), input.Text()) {
+				fmt.Println(lines.Text())
+				break
+			}
+		}
+	}
 }
